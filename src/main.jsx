805696ee25e9e70db542e62e008ac2c552b7c0f0
Nate LaClaire <nate@natelaclaire.com>
2025-11-14 20:13:16 -0500

    Add GitHub Pages deployment workflow

--- src/main.jsx
+++ src/main.jsx
@@ -21,7 +21,9 @@ const router = createBrowserRouter([
       { path: 'modules/:slug', element: <Module /> }
     ]
   }
-])
+], {
+  basename: '/int-ame-264'
+})
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
