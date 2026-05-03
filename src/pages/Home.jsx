0d308ce021a650e1cf18b93c45d6954cf2327850
Nate LaClaire <nate@natelaclaire.com>
2026-05-03 01:15:37 -0400

    Fixed home page title

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ export default function Home() {
     <section className="home-hero">
       <div className="home-text">
         <h1>INT/AME 264</h1>
-        <p className="lead">The History and Culture of Video Games</p>
+        <p className="lead">The History and Culture of Video Gaming</p>
         <p>
           This course explores the evolution of video games, their cultural and societal impact, and their role beyond entertainment. Students will analyze key milestones, ethical concerns, and technological advancements while examining gaming's influence on identity, education, and industry trends. The course includes historical studies, critical discussions, and hands-on engagement with gaming culture.
         </p>
